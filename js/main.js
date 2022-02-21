7d339084111d79392d4650fcb24bc50dd0dc485f
FrancescoCapriolo <francesco.capriolo92@gmail.com>
2022-02-21 16:41:52 +0100

    correzione e aggiunta di bottone

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let firstName = prompt("Please enter your name");
+/* let firstName = prompt("Please enter your name", "Francesco");
 document.getElementById("my-name").innerHTML = firstName;
 
 let surName = prompt("Please enter your surname", "Capriolo");
@@ -8,4 +8,29 @@ let color = prompt("Please enter your favorite color", "Blue");
 document.getElementById("my-color").innerHTML = color;
 
 document.getElementById("demo").innerHTML =
-    "Hello " + firstName + surName + color + "! How are you today?";
+    "Hello " + firstName + surName + color + "21"; */
+
+function myFunction() {
+    let firstName = prompt("Please enter your name", "Francesco");
+
+    if (firstName != null) {
+        document.getElementById("my-name").innerHTML = firstName;
+    }
+
+    document.getElementById("demo-surname").innerHTML = "Cognome?";
+
+    let surName = prompt("Please enter your surname", "Capriolo");
+
+    if (surName != null) {
+        document.getElementById("my-surname").innerHTML = surName;
+    }
+    document.getElementById("demo-color").innerHTML = "Colore preferito?";
+
+    let color = prompt("Please enter your favorite color", "Blue");
+
+    if (color != null) {
+        document.getElementById("my-color").innerHTML = color;
+    }
+    document.getElementById("demo").innerHTML =
+        "Hello " + firstName + surName + color + "21";
+}
